da054b9a327d1e5afa03ae0f23895017ed19d7bd
TRKL444 <kalielmendesdasilva@gmail.com>
2025-06-04 13:55:50 -0400

    primeiro commit do laravel

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,79 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div class="mt-4 relative flex items-center gap-2">
+    <!-- Ícone posicionado fora do input -->
+        <img src="{{ asset('./img/user-solid.svg') }}" alt="{{ config('app.name', 'Icon') }}" class="h-6 w-6">
+
+    <!-- Campo de entrada com label e erro -->
+        <div class="w-full">
+        <x-input-label for="email" :value="__('')" class="mb-1" />
+        <x-text-input  
+            id="email" 
+            class="block w-full" 
+            type="email" 
+            name="email" 
+            :value="old('email')" 
+            required 
+            autofocus 
+            autocomplete="username"
+            placeholder="Email"
+        />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+        </div>
+
+
+        <!-- Password -->
+        <div class="mt-4 relative flex items-center gap-2">
+            <img src="{{ asset('./img/lock-solid (1).svg') }}" alt="{{ config('app.name', default: 'Icon') }}" class="h-6 w-6" >
+            <div class="w-full" >
+                <x-input-label for="password" :value="__('')" />
+                <x-text-input id="password" class="block w-full"
+                            type="password"
+                            name="password"
+                            required 
+                            autocomplete="current-password"
+                            placeholder="Senha" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+            
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Lembrar senha') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-between mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Esqueceu sua senha?') }}
+                </a>
+            @endif
+
+            <div class="flex ms-2" >
+                 @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-3">
+                                Register
+                            </a>
+                        @endif
+            </div>
+                
+
+            <x-primary-button class="ms-3">
+                {{ __('Login') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
